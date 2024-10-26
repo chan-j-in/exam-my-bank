264ecbd6e38cffdcb45b8032ce9d5ac25f21d802
l__o_ <genius8111@naver.com>
2024-10-27 01:59:03 +0900

    Feat: 회원가입 중복 예외처리 추가

--- src/main/java/exam/myBank/service/MemberService.java
+++ src/main/java/exam/myBank/service/MemberService.java
@@ -3,25 +3,47 @@ package exam.myBank.service;
 import exam.myBank.domain.entity.Member;
 import exam.myBank.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public Member join(String username, String email, String password) {
-        Member member = Member.builder()
-                .username(username)
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .build();
-        memberRepository.save(member);
 
-        return member;
+        try {
+            Member member = Member.builder()
+                    .username(username)
+                    .email(email)
+                    .password(passwordEncoder.encode(password))
+                    .build();
+            memberRepository.save(member);
+            return member;
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("중복된 이메일이나 회원명입니다.");
+        }
+    }
+
+    private boolean emailValidation(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isEmpty()) return true;
+        return false;
+    }
+
+    private boolean usernameValidation(String username) {
+        Optional<Member> member = memberRepository.findByUsername(username);
+        if (member.isEmpty()) return true;
+        return false;
     }
 
 }
