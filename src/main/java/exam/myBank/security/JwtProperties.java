424b3d0e6de1790f9eddccf447f3d35f6c95858e
l__o_ <genius8111@naver.com>
2024-10-30 01:47:12 +0900

    Fix: 인증/인가 구현 초기화

--- src/main/java/exam/myBank/security/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package exam.myBank.security;
-
-public interface JwtProperties {
-    String SECRET = "asdfghjkl";
-    int EXPIRATION_TIME = 864000000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
