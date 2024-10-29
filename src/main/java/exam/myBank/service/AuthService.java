9dec31b0909187962fd8f96ed593b3494bec50e0
l__o_ <genius8111@naver.com>
2024-10-29 16:32:36 +0900

    Fix: 인증/인가 service는 AuthService로 인계

---
+++ src/main/java/exam/myBank/service/AuthService.java
@@ -0,0 +1,64 @@
+package exam.myBank.service;
+
+import exam.myBank.domain.entity.Member;
+import exam.myBank.domain.repository.MemberRepository;
+import exam.myBank.dto.memberDto.SignInRequestDto;
+import exam.myBank.dto.memberDto.SignupRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public String signIn(SignInRequestDto requestDto) {
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestDto.getUsername(),
+                            requestDto.getPassword()
+                    )
+            );
+
+            return "로그인 성공 id: "+requestDto.getUsername();
+        } catch (AuthenticationException e) {
+            throw new IllegalArgumentException("아이디 또는 비밀번호가 잘못되었습니다.");
+        }
+    }
+
+    @Transactional
+    public Member join(SignupRequestDto requestDto) {
+
+        if (usernameValidation(requestDto.getUsername())) throw new IllegalArgumentException("이미 사용 중인 사용자 이름입니다.");
+        if (emailValidation(requestDto.getUsername())) throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        if (!requestDto.getPassword().equals(requestDto.getPasswordCheck())) throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+
+        Member member = Member.builder()
+                .username(requestDto.getUsername())
+                .email(requestDto.getEmail())
+                .password(passwordEncoder.encode(requestDto.getPassword()))
+                .build();
+        return memberRepository.save(member);
+    }
+
+    private boolean emailValidation(String email) {
+        return memberRepository.findByEmail(email).isPresent();
+    }
+
+    private boolean usernameValidation(String username) {
+        return memberRepository.findByUsername(username).isPresent();
+    }
+
+}
--- src/main/java/exam/myBank/service/MemberService.java
+++ src/main/java/exam/myBank/service/MemberService.java
@@ -1,18 +1,10 @@
 package exam.myBank.service;
 
-import exam.myBank.domain.entity.Account;
-import exam.myBank.domain.entity.Member;
-import exam.myBank.domain.repository.AccountRepository;
 import exam.myBank.domain.repository.MemberRepository;
-import exam.myBank.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,27 +12,7 @@ import java.util.Optional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-    private final AccountRepository accountRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    @Transactional
-    public Member join(String username, String email, String password) {
-
-        Member member = Member.builder()
-                .username(username)
-                .email(email)
-                .password(passwordEncoder.encode(password))
-                .build();
-        return memberRepository.save(member);
-    }
-
-    public boolean emailValidation(String email) {
-        return memberRepository.findByEmail(email).isPresent();
-    }
-
-    public boolean usernameValidation(String username) {
-        return memberRepository.findByUsername(username).isPresent();
-    }
 
     @Transactional
     public void clear() {
