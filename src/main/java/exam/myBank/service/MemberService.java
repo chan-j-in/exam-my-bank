11bc96e636182f52cd9fd0622b8722d65aa2a81f
l__o_ <genius8111@naver.com>
2024-10-30 01:53:16 +0900

    Feat: 회원가입 구현

--- src/main/java/exam/myBank/service/MemberService.java
+++ src/main/java/exam/myBank/service/MemberService.java
@@ -2,18 +2,37 @@ package exam.myBank.service;
 
 import exam.myBank.domain.entity.Member;
 import exam.myBank.domain.repository.MemberRepository;
+import exam.myBank.exception.AppException;
+import exam.myBank.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
-
+@Slf4j
 @Service
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class MemberService extends BaseService {
+public class MemberService {
 
-    public MemberService(MemberRepository memberRepository) {
-        super(memberRepository);
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public String join(String username, String password) {
+        log.info("username : {}",username);
+        log.info("password : {}",password);
+        memberRepository.findByUsername(username).ifPresent(user -> {
+            throw new AppException(ErrorCode.USERNAME_DUPLICATED, username+"은(는) 이미 있습니다.");
+        });
+
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .build();
+        memberRepository.save(member);
+
+        return "success";
     }
 
     public Member findMemberById(Long memberId) {
@@ -27,28 +46,21 @@ public class MemberService extends BaseService {
         return memberRepository.findAll();
     }
 
-    @Transactional
-    public Member updateEmail(String email) {
 
-        Member member = getCurrentMember();
-        member.updateEmail(email);
-        return memberRepository.save(member);
-    }
-
-    @Transactional
-    public Member updatePassword(String password) {
-
-        Member member = getCurrentMember();
-        member.updatePassword(password);
-        return memberRepository.save(member);
-    }
-
-    @Transactional
-    public void delete() {
-
-        Member member = getCurrentMember();
-        memberRepository.delete(member);
-    }
+//    @Transactional
+//    public Member updatePassword(String password) {
+//
+//        Member member = getCurrentMember();
+//        member.updatePassword(password);
+//        return member;
+//    }
+//
+//    @Transactional
+//    public void delete() {
+//
+//        Member member = getCurrentMember();
+//        memberRepository.delete(member);
+//    }
 
     @Transactional
     public void clear() {
