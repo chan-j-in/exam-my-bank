28d75300643722cb161f88d6b405e17eefbe8697
l__o_ <genius8111@naver.com>
2024-10-29 17:14:02 +0900

    Feat: 회원 조회, 정보 변경, 삭제 sevice 구현

--- src/main/java/exam/myBank/service/MemberService.java
+++ src/main/java/exam/myBank/service/MemberService.java
@@ -1,18 +1,54 @@
 package exam.myBank.service;
 
+import exam.myBank.domain.entity.Member;
 import exam.myBank.domain.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
-@RequiredArgsConstructor
+
 @Service
 @Transactional(readOnly = true)
-public class MemberService {
+public class MemberService extends BaseService {
 
-    private final MemberRepository memberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        super(memberRepository);
+    }
 
+    public Member findMemberById(Long memberId) {
+
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
+    }
+
+    public List<Member> findMembers() {
+
+        return memberRepository.findAll();
+    }
+
+    @Transactional
+    public Member updateEmail(String email) {
+
+        Member member = getCurrentMember();
+        member.updateEmail(email);
+        return memberRepository.save(member);
+    }
+
+    @Transactional
+    public Member updatePassword(String password) {
+
+        Member member = getCurrentMember();
+        member.updatePassword(password);
+        return memberRepository.save(member);
+    }
+
+    @Transactional
+    public void delete() {
+
+        Member member = getCurrentMember();
+        memberRepository.delete(member);
+    }
 
     @Transactional
     public void clear() {
