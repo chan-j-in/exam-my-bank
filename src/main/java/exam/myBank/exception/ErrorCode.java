502cdac4a6445aed1a3b12936db053de4b83c0ff
l__o_ <genius8111@naver.com>
2024-10-30 14:43:53 +0900

    Feat: AppException 에서 사용할 ErrorCode 생성

--- src/main/java/exam/myBank/exception/ErrorCode.java
+++ src/main/java/exam/myBank/exception/ErrorCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USERNAME_DUPLICATED(HttpStatus.CONFLICT, "");
+    USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+    USER_NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "사용자가 인증되지 않았습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    ;
 
     private HttpStatus httpStatus;
     private String message;
