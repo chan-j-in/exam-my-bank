1195af21fbbf2ed6e29ee3b3c561852112921cae
l__o_ <genius8111@naver.com>
2024-10-31 02:05:46 +0900

    Feat: 계좌 이체 dto

---
+++ src/main/java/exam/myBank/domain/dto/accountDto/TransferRequestDto.java
@@ -0,0 +1,16 @@
+package exam.myBank.domain.dto.accountDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransferRequestDto {
+
+    private String myAccountNum;
+    private String targetAccountNum;
+    private Long amount;
+
+}
