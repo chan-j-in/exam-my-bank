154202255f2714236e91c1f17f8654938caf1eba
l__o_ <genius8111@naver.com>
2024-10-31 02:05:59 +0900

    Feat: Error Code 추가

--- src/main/java/exam/myBank/exception/ErrorCode.java
+++ src/main/java/exam/myBank/exception/ErrorCode.java
@@ -10,11 +10,12 @@ public enum ErrorCode {
     USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
-    USER_NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, "사용자가 인증되지 않았습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, "계좌를 찾을 수 없습니다."),
+    USER_NOT_AUTHENTICATED(HttpStatus.UNAUTHORIZED, ""),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
+    ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
     INVALID_AMOUNT(HttpStatus.BAD_REQUEST, ""),
-    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "")
+    INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, ""),
+    SAME_ACCOUNT_TRANSFER(HttpStatus.BAD_REQUEST, "")
     ;
 
     private HttpStatus httpStatus;
