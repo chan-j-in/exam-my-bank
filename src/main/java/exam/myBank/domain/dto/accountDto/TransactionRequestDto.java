a5049fae0354e5f11e9aa45b1378a29589b453a8
l__o_ <genius8111@naver.com>
2024-10-31 01:09:24 +0900

    Feat: 입/출금 request dto

---
+++ src/main/java/exam/myBank/domain/dto/accountDto/TransactionRequestDto.java
@@ -0,0 +1,14 @@
+package exam.myBank.domain.dto.accountDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionRequestDto {
+
+    private String accountNum;
+    private Long amount;
+}
