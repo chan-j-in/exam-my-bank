a731f738c0338f527107879c4742e8c5a660c13e
l__o_ <genius8111@naver.com>
2024-10-31 20:16:27 +0900

    Feat: 계좌 이름 변경 request dto

---
+++ src/main/java/exam/myBank/domain/dto/accountDto/UpdateAccountNameRequestDto.java
@@ -0,0 +1,14 @@
+package exam.myBank.domain.dto.accountDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateAccountNameRequestDto {
+
+    private String accountNum;
+    private String accountName;
+}
