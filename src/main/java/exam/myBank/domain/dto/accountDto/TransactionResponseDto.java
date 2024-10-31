26f6c1315465fd485b8772b2f76cb8d03dadd22a
l__o_ <genius8111@naver.com>
2024-10-31 14:51:26 +0900

    Feat: 거래 내역 response dto

---
+++ src/main/java/exam/myBank/domain/dto/accountDto/TransactionResponseDto.java
@@ -0,0 +1,20 @@
+package exam.myBank.domain.dto.accountDto;
+
+import exam.myBank.type.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionResponseDto {
+
+    private TransactionType transactionType;
+    private Long amount;
+    private Long balanceAfterTransaction;
+    private LocalDateTime transactionDate;
+    private String targetAccountNum;
+}
